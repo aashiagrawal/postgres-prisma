2d516e191a91b174fd991aa733c7feec598d75ac
aashiagrawal <aashia2@illinois.edu>
2023-11-11 16:39:28 -0600

    fixed dashboard and table files

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,71 +1,71 @@
-// import { DashboardItem } from "@/types/dashboardItem";
-// import React, {useState, useEffect} from "react";
-// import Chart from "./chart";
+import { DashboardItem } from "@/types/dashboardItem";
+import React, {useState, useEffect} from "react";
+import Chart from "./chart";
 
-// const Dashboard = (props: { name: string; containerStyle: any; onClickDashboardItem: any }) => {
+const Dashboard = (props: { name: string; containerStyle: any; onClickDashboardItem: any }) => {
 
-//     const dashName = props.name
-//     const containerStyle = props.containerStyle
-//     const onClickDashboardItem = props.onClickDashboardItem
+    const dashName = props.name
+    const containerStyle = props.containerStyle
+    const onClickDashboardItem = props.onClickDashboardItem
 
-//     return (
-//         <div>
-//             <div className="flex items-center space-x-4">
-//                 <div className="space-y-1">
-//                     <p className="font-medium leading-none">{dashName}</p>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Dashboard
-
-import React, { useState } from 'react';
-import Chart from './chart';
-import { DashboardItem } from '@/types/dashboardItem';
-
-interface DashboardProps {
-  name: string;
-  containerStyle: React.CSSProperties;
-  onClickDashboardItem: (dashboardItem: DashboardItem) => void;
+    return (
+        <div>
+            <div className="flex items-center space-x-4">
+                <div className="space-y-1">
+                    <p className="font-medium leading-none">{dashName}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-const Dashboard: React.FC<DashboardProps> = ({ name, containerStyle, onClickDashboardItem }) => {
-  const [charts, setCharts] = useState<DashboardItem[]>([]);
+export default Dashboard
 
-//   useEffect(() => {
-//     // Replace this URL with your actual endpoint
-//     const fetchUrl = `/api/dashboard/${name}`;
+// import React, { useState } from 'react';
+// import Chart from './chart';
+// import { DashboardItem } from '@/types/dashboardItem';
 
-//     const fetchDashboardData = async () => {
-//       try {
-//         const response = await axios.get<DashboardItem[]>(fetchUrl);
-//         setCharts(response.data);
-//       } catch (error) {
-//         console.error('Error fetching dashboard data:', error);
-//       }
-//     };
+// interface DashboardProps {
+//   name: string;
+//   containerStyle: React.CSSProperties;
+//   onClickDashboardItem: (dashboardItem: DashboardItem) => void;
+// }
 
-//     fetchDashboardData();
-//   }, [name]);
+// const Dashboard: React.FC<DashboardProps> = ({ name, containerStyle, onClickDashboardItem }) => {
+//   const [charts, setCharts] = useState<DashboardItem[]>([]);
 
-  return (
-    <div style={containerStyle}>
-      <div className="flex items-center space-x-4">
-        <div className="space-y-1">
-          <p className="font-medium leading-none">{name}</p>
-        </div>
-      </div>
-      <div>
-        {charts.map((chart) => (
-          <div key={chart.id} onClick={() => onClickDashboardItem(chart)}>
-            {/* <Chart chartData={chart} /> */}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+// //   useEffect(() => {
+// //     // Replace this URL with your actual endpoint
+// //     const fetchUrl = `/api/dashboard/${name}`;
 
-export default Dashboard;
+// //     const fetchDashboardData = async () => {
+// //       try {
+// //         const response = await axios.get<DashboardItem[]>(fetchUrl);
+// //         setCharts(response.data);
+// //       } catch (error) {
+// //         console.error('Error fetching dashboard data:', error);
+// //       }
+// //     };
+
+// //     fetchDashboardData();
+// //   }, [name]);
+
+//   return (
+//     <div style={containerStyle}>
+//       <div className="flex items-center space-x-4">
+//         <div className="space-y-1">
+//           <p className="font-medium leading-none">{name}</p>
+//         </div>
+//       </div>
+//       <div>
+//         {charts.map((chart) => (
+//           <div key={chart.id} onClick={() => onClickDashboardItem(chart)}>
+//             {/* <Chart chartData={chart} /> */}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
--- components/table.tsx
+++ components/table.tsx
@@ -1,81 +1,85 @@
-// import prisma from '@/lib/prisma'
-// import Dashboard from './dashboard'
+import prisma from '@/lib/prisma'
+import Dashboard from './dashboard'
 
-// export default async function Table() {
-//   const dashboards = await prisma.dashboard.findMany()
+export default async function Table() {
+  const dashboards = await prisma.dashboard.findMany()
 
-//   console.log(dashboards)
-
-//   return (
-//     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
-//       <div className="flex justify-between items-center mb-4">
-//         <div className="space-y-1">
-//           <h2 className="text-xl font-semibold">Dashboards</h2>
-//           <p className="text-sm text-gray-500">
-//           </p>
-//         </div>
-//       </div>
-//       <div className="divide-y divide-gray-900/5">
-//         {dashboards.map((dash) => (
-//           <div
-//             key = {dash.name}
-//             className="flex items-center justify-between py-3"
-//           >
-//             <Dashboard name={dash.name} containerStyle={"React.CSSProperties"} onClickDashboardItem={(dashboardItem: DashboardItem) => void}></Dashboard>
-//             {/* <div className="flex items-center space-x-4">
-//               <div className="space-y-1">
-//                 <p className="font-medium leading-none">{dash.name}</p>
-//               </div>
-//             </div> */}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-'use client'
-import React, { useState, useEffect } from 'react';
-import Dashboard from './dashboard';
-import prisma from '@/lib/prisma';
-import { DashboardItem } from '@/types/dashboardItem';
-
-const Table = () => {
-  const [dashboards, setDashboards] = useState<any[]>([]); // Update the type based on your actual data
-
-  useEffect(() => {
-    const fetchDashboards = async () => {
-      const data = await prisma.dashboard.findMany();
-      setDashboards(data);
-    };
-
-    fetchDashboards();
-  }, []);
-
-  const handleDashboardItemClick = (dashboardItem: DashboardItem) => {
-    // Handle the click event here
-    console.log(dashboardItem);
-  };
+  console.log(dashboards)
 
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">Dashboards</h2>
-          <p className="text-sm text-gray-500"></p>
+          <p className="text-sm text-gray-500">
+          </p>
         </div>
       </div>
       <div className="divide-y divide-gray-900/5">
         {dashboards.map((dash) => (
-          <Dashboard 
-            key={dash.name}
-            name={dash.name}
-            containerStyle={{}} // Add your CSS properties here
-            onClickDashboardItem={handleDashboardItemClick}
-          />
+          <div
+            key = {dash.name}
+            className="flex items-center justify-between py-3"
+          >
+            <Dashboard name={dash.name} containerStyle={"React.CSSProperties"} onClickDashboardItem={""}></Dashboard>
+            {/* <div className="flex items-center space-x-4">
+              <div className="space-y-1">
+                <p className="font-medium leading-none">{dash.name}</p>
+              </div>
+            </div> */}
+          </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Table;
+
+
+
+// 'use client'
+// import React, { useState, useEffect } from 'react';
+// import Dashboard from './dashboard';
+// import prisma from '@/lib/prisma';
+// import { DashboardItem } from '@/types/dashboardItem';
+
+// const Table = () => {
+//   const [dashboards, setDashboards] = useState<any[]>([]); // Update the type based on your actual data
+
+//   useEffect(() => {
+//     const fetchDashboards = async () => {
+//       const data = await prisma.dashboard.findMany();
+//       setDashboards(data);
+//     };
+
+//     fetchDashboards();
+//   }, []);
+
+//   const handleDashboardItemClick = (dashboardItem: DashboardItem) => {
+//     // Handle the click event here
+//     console.log(dashboardItem);
+//   };
+
+//   return (
+//     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
+//       <div className="flex justify-between items-center mb-4">
+//         <div className="space-y-1">
+//           <h2 className="text-xl font-semibold">Dashboards</h2>
+//           <p className="text-sm text-gray-500"></p>
+//         </div>
+//       </div>
+//       <div className="divide-y divide-gray-900/5">
+//         {dashboards.map((dash) => (
+//           <Dashboard 
+//             key={dash.name}
+//             name={dash.name}
+//             containerStyle={{}} // Add your CSS properties here
+//             onClickDashboardItem={handleDashboardItemClick}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Table;
