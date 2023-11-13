68678f38853d1933e58d8fb7bbc54cc77924eb0f
aashiagrawal <aashia2@illinois.edu>
2023-11-13 09:36:33 -0600

    commented out barchart component

--- components/barchart.tsx
+++ components/barchart.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {BarChart, } from 'recharts';
+// import React from 'react'
+// import {BarChart, } from 'recharts';
 
-const barchart = () => {
-  return (
-    <BarChart width={730} height={250} data={data}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
-  <YAxis />
-  <Tooltip />
-  <Legend />
-  <Bar dataKey="pv" fill="#8884d8" />
-  <Bar dataKey="uv" fill="#82ca9d" />
-</BarChart>
-  )
-}
+// const barchart = () => {
+//   return (
+//     <BarChart width={730} height={250} data={data}>
+//   <CartesianGrid strokeDasharray="3 3" />
+//   <XAxis dataKey="name" />
+//   <YAxis />
+//   <Tooltip />
+//   <Legend />
+//   <Bar dataKey="pv" fill="#8884d8" />
+//   <Bar dataKey="uv" fill="#82ca9d" />
+// </BarChart>
+//   )
+// }
 
-export default barchart
+// export default barchart
